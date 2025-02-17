9fb1b8364bbb5b8e5a91c1b557cddaeb49af4cf5
Henry Nguyen <nguyenhenry682@gmail.com>
2025-02-16 21:58:58 -0600

    initial commit for servicereg

---
+++ servicereg/src/main/java/com/re_ride/servicereg/ServiceregApplication.java
@@ -0,0 +1,13 @@
+package com.re_ride.servicereg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiceregApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServiceregApplication.class, args);
+	}
+
+}
