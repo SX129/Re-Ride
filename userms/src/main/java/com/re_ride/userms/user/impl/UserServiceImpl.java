adb4fc6cec28248ee69850173c5ae06b8d14424f
Henry Nguyen <nguyenhenry682@gmail.com>
2025-02-17 16:54:04 -0600

    removed unused import

--- userms/src/main/java/com/re_ride/userms/user/impl/UserServiceImpl.java
+++ userms/src/main/java/com/re_ride/userms/user/impl/UserServiceImpl.java
@@ -3,7 +3,6 @@ package com.re_ride.userms.user.impl;
 import com.re_ride.userms.user.User;
 import com.re_ride.userms.user.UserRepository;
 import com.re_ride.userms.user.UserService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
