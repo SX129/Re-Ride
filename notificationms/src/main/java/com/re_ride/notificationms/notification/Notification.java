7bcdefc6d3797cc2a94e679ab9cb5e00bac1e3a6
Henry Nguyen <nguyenhenry682@gmail.com>
2025-02-17 22:12:53 -0600

    added attributes

--- notificationms/src/main/java/com/re_ride/notificationms/notification/Notification.java
+++ notificationms/src/main/java/com/re_ride/notificationms/notification/Notification.java
@@ -1,4 +1,74 @@
 package com.re_ride.notificationms.notification;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+import jakarta.persistence.*;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "notifications")
 public class Notification {
+    public enum NotificationType {
+        RIDE_UPDATE,
+        PAYMENT_RECEIPT,
+        PROMOTION
+    }
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long notificationId;
+
+    private String message;
+
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+    private LocalDateTime createdAt;
+
+    private Long userId;
+
+    public Notification(){}
+
+    public Notification(Long notificationId, String message, LocalDateTime createdAt) {
+        this.notificationId = notificationId;
+        this.message = message;
+        this.createdAt = createdAt;
+    }
+
+    @PrePersist
+    public void prePersist() {
+        if (this.createdAt == null) {
+            this.createdAt = LocalDateTime.now();
+        }
+    }
+
+    public Long getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Long userId) {
+        this.userId = userId;
+    }
+
+    public Long getNotificationId() {
+        return notificationId;
+    }
+
+    public void setNotificationId(Long notificationId) {
+        this.notificationId = notificationId;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
 }
