548cc773f11b6d9f61876c70c0f8aae6aa30b958
Henry Nguyen <nguyenhenry682@gmail.com>
2025-02-17 20:29:34 -0600

    added dto for vehicle

---
+++ userms/src/main/java/com/re_ride/userms/vehicle/dto/VehicleDTO.java
@@ -0,0 +1,78 @@
+package com.re_ride.userms.vehicle.dto;
+
+import com.re_ride.userms.vehicle.Vehicle;
+
+public class VehicleDTO {
+    private Long userId;
+    private String make;
+    private String model;
+    private Integer year;
+    private String licensePlate;
+    private String color;
+    private Integer capacity;
+    private Vehicle.VehicleType vehicleType;
+
+    public Long getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Long userId) {
+        this.userId = userId;
+    }
+
+    public String getMake() {
+        return make;
+    }
+
+    public void setMake(String make) {
+        this.make = make;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public Integer getYear() {
+        return year;
+    }
+
+    public void setYear(Integer year) {
+        this.year = year;
+    }
+
+    public String getLicensePlate() {
+        return licensePlate;
+    }
+
+    public void setLicensePlate(String licensePlate) {
+        this.licensePlate = licensePlate;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public Integer getCapacity() {
+        return capacity;
+    }
+
+    public void setCapacity(Integer capacity) {
+        this.capacity = capacity;
+    }
+
+    public Vehicle.VehicleType getVehicleType() {
+        return vehicleType;
+    }
+
+    public void setVehicleType(Vehicle.VehicleType vehicleType) {
+        this.vehicleType = vehicleType;
+    }
+}
---
+++ userms/src/main/java/com/re_ride/userms/vehicle/mapper/VehicleMapper.java
@@ -0,0 +1,24 @@
+package com.re_ride.userms.vehicle.mapper;
+
+import com.re_ride.userms.driver.Driver;
+import com.re_ride.userms.driver.dto.DriverDTO;
+import com.re_ride.userms.vehicle.Vehicle;
+import com.re_ride.userms.vehicle.dto.VehicleDTO;
+
+public class VehicleMapper {
+    public static VehicleDTO mapVehicleDto(Vehicle vehicle){
+        VehicleDTO vehicleDTO = new VehicleDTO();
+
+        vehicleDTO.setMake(vehicle.getMake());
+        vehicleDTO.setModel(vehicle.getModel());
+        vehicleDTO.setVehicleType(vehicle.getVehicleType());
+        vehicleDTO.setCapacity(vehicle.getCapacity());
+        vehicleDTO.setColor(vehicle.getColor());
+        vehicleDTO.setYear(vehicle.getYear());
+        vehicleDTO.setLicensePlate(vehicle.getLicensePlate());
+
+        vehicleDTO.setUserId(vehicle.getDriver().getUserId());
+
+        return vehicleDTO;
+    }
+}
