3b2ad8ee15a846b0369d47a33fbbcb01f09c7854
Henry Nguyen <nguyenhenry682@gmail.com>
2025-02-19 09:47:34 -0600

    added repository layer

--- notificationms/src/main/java/com/re_ride/notificationms/notification/NotificationRepository.java
+++ notificationms/src/main/java/com/re_ride/notificationms/notification/NotificationRepository.java
@@ -2,5 +2,8 @@ package com.re_ride.notificationms.notification;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface NotificationRepository extends JpaRepository<Notification, Long> {
+    List<Notification> findByUserId(Long userId);
 }
