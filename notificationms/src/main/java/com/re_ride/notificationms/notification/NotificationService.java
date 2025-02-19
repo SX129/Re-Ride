575c21d6e99f5da3aa9575f2ac86ab081ab95c1c
Henry Nguyen <nguyenhenry682@gmail.com>
2025-02-19 09:47:48 -0600

    added business logic

--- notificationms/src/main/java/com/re_ride/notificationms/notification/NotificationService.java
+++ notificationms/src/main/java/com/re_ride/notificationms/notification/NotificationService.java
@@ -1,4 +1,17 @@
 package com.re_ride.notificationms.notification;
 
+import com.re_ride.notificationms.notification.dto.UserDTO;
+
+import java.util.List;
+
 public interface NotificationService {
+    List<Notification> getAllNotificationsByUserId(Long userId);
+
+    List<Notification> getAllNotificationTypeByUserId(Long userId, String notificationType);
+
+    Notification createNotification(Notification notification);
+
+    boolean deleteNotification(Long userId, Long notificationId);
+
+    UserDTO getUser(Long userId);
 }
--- notificationms/src/main/java/com/re_ride/notificationms/notification/impl/NotificationServiceImpl.java
+++ notificationms/src/main/java/com/re_ride/notificationms/notification/impl/NotificationServiceImpl.java
@@ -1,7 +1,81 @@
 package com.re_ride.notificationms.notification.impl;
 
+import com.re_ride.notificationms.notification.Notification;
+import com.re_ride.notificationms.notification.NotificationRepository;
+import com.re_ride.notificationms.notification.client.UserClient;
+import com.re_ride.notificationms.notification.dto.UserDTO;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class NotificationServiceImpl {
+    private NotificationRepository notificationRepository;
+    private UserClient userClient;
+
+    public NotificationServiceImpl(NotificationRepository notificationRepository, UserClient userClient) {
+        this.notificationRepository = notificationRepository;
+        this.userClient = userClient;
+    }
+
+    public UserDTO getUser(Long userId){
+        UserDTO user = userClient.getUserById(userId);
+
+        if(user == null){
+            return null;
+        }
+
+        return user;
+    }
+
+    public List<Notification> getAllNotificationsByUserId(Long userId){
+        if(getUser(userId) == null){
+            return null;
+        }
+
+        return notificationRepository.findByUserId(userId);
+    }
+
+    public List<Notification> getAllNotificationTypeByUserId(Long userId, String notificationType) {
+        try {
+            if(getUser(userId) == null){
+                return null;
+            }
+
+            Notification.NotificationType type = Notification.NotificationType.valueOf(notificationType);
+
+            return notificationRepository.findByUserId(userId).stream()
+                    .filter(notification -> notification.getNotificationType() == type)
+                    .collect(Collectors.toList());
+        } catch (IllegalArgumentException e) {
+            return null;
+        }
+    }
+
+    public Notification createNotification(Notification notification){
+        if(getUser(notification.getUserId()) == null){
+            return null;
+        }
+        notificationRepository.save(notification);
+
+        return notification;
+    }
+
+    public boolean deleteNotification(Long userId, Long notificationId) {
+        if (getUser(userId) == null) {
+            return false;
+        }
+
+        Notification notification = notificationRepository.findById(notificationId).orElse(null);
+
+        if (notification != null && notification.getUserId().equals(userId)) {
+            notificationRepository.deleteById(notificationId);
+            return true;
+        }
+
+        return false;
+    }
+
+
 }
