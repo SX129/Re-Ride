328688c0b8d743d0bdf86a943a0f6b7eccf72eea
Henry Nguyen <nguyenhenry682@gmail.com>
2025-02-19 22:32:46 -0600

    initial commit for payment service

---
+++ paymentms/src/main/java/com/re_ride/paymentms/PaymentmsApplication.java
@@ -0,0 +1,13 @@
+package com.re_ride.paymentms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaymentmsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PaymentmsApplication.class, args);
+	}
+
+}
