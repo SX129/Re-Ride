0c14524d13550b67eab3955aae32d9f32a7553eb
Henry Nguyen <nguyenhenry682@gmail.com>
2025-02-20 13:17:44 -0600

    added client for user service

---
+++ paymentms/src/main/java/com/re_ride/paymentms/payment/dto/UserDTO.java
@@ -0,0 +1,22 @@
+package com.re_ride.paymentms.payment.dto;
+
+public class UserDTO {
+    private Long userId;
+    private String userType;
+
+    public Long getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Long userId) {
+        this.userId = userId;
+    }
+
+    public String getUserType() {
+        return userType;
+    }
+
+    public void setUserType(String userType) {
+        this.userType = userType;
+    }
+}
