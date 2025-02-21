b9b8f7592a368feb081e1ebe28dc7f3a7a0421fe
Henry Nguyen <nguyenhenry682@gmail.com>
2025-02-20 19:49:08 -0600

    initial commit for subscription service

---
+++ subscriptionms/src/main/java/com/re_ride/subscriptionms/SubscriptionmsApplication.java
@@ -0,0 +1,13 @@
+package com.re_ride.subscriptionms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SubscriptionmsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SubscriptionmsApplication.class, args);
+	}
+
+}
---
+++ subscriptionms/src/test/java/com/re_ride/subscriptionms/SubscriptionmsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.re_ride.subscriptionms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SubscriptionmsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
