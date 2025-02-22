38f3abb70bb13d9b298c5935d4af499ee26a7df9
Henry Nguyen <nguyenhenry682@gmail.com>
2025-02-22 01:00:23 -0600

    added null check

--- subscriptionms/src/main/java/com/re_ride/subscriptionms/subscription/mapper/SubscriptionMapper.java
+++ subscriptionms/src/main/java/com/re_ride/subscriptionms/subscription/mapper/SubscriptionMapper.java
@@ -1,5 +1,6 @@
 package com.re_ride.subscriptionms.subscription.mapper;
 
+import com.re_ride.subscriptionms.route.Route;
 import com.re_ride.subscriptionms.subscription.Subscription;
 import com.re_ride.subscriptionms.subscription.dto.SubscriptionDTO;
 
@@ -10,13 +11,18 @@ public class SubscriptionMapper {
         subscriptionDTO.setSubscriptionId(subscription.getSubscriptionId());
         subscriptionDTO.setUserId(subscription.getUserId());
         subscriptionDTO.setPaymentId(subscription.getPaymentId());
-        subscriptionDTO.setRouteId(subscription.getRoute().getRouteId());
         subscriptionDTO.setSubscriptionAmount(subscription.getSubscriptionAmount());
         subscriptionDTO.setPickUpDate(SubscriptionDTO.PickUpDate.valueOf(subscription.getPickUpDate().name()));
         subscriptionDTO.setPickUpTime(subscription.getPickUpTime());
         subscriptionDTO.setSubscriptionStatus(SubscriptionDTO.SubscriptionStatus.valueOf(subscription.getSubscriptionStatus().name()));
         subscriptionDTO.setCreatedAt(subscription.getCreatedAt());
         subscriptionDTO.setUpdatedAt(subscription.getUpdatedAt());
+
+        Route route = subscription.getRoute();
+
+        if(route != null){
+            subscriptionDTO.setRouteId(subscription.getRoute().getRouteId());
+        }
 
         return subscriptionDTO;
     }
